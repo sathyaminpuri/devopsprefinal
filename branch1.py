140d7637ab5855ce1090adc4d2896802e865472e
kush1235 <arcotkushal@gmail.com>
2024-10-21 13:49:16 +0530

    committing the changes

---
+++ branch1.py
@@ -0,0 +1,8 @@
+# Simple Python program to greet the user
+
+# Get user input
+name = input("What is your name? ")
+age = input("How old are you? ")
+
+# Print a greeting message
+print(f"Hello, {name}! You are {age} years old.")
